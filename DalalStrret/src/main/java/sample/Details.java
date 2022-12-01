7ce5c3ae67686fd14d5342a33d05404402c2e157
sevakselokar <sevakselokar@gmail.com>
2022-12-01 17:10:52 +0530

    Detail
    Co-Authored-By: pankajharne <119295080+pankajharne@users.noreply.github.com>

---
+++ DalalStrret/src/main/java/sample/Details.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("name");
+System.out.println("Sirname");
+System.out.print("Email");
+	}
+
+}
